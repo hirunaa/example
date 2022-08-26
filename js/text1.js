e7c9097cd03e55b7cd68a51118fb461d0a16cd15
hirunaa <tmfrl040622@naver.com>
2022-08-26 17:23:13 +0900

    js 수정

--- js/text1.js
+++ js/text1.js
@@ -36,19 +36,24 @@ $( document ).ready(function() {
     // input창에 "유"입력 시 연관검색어 출력. 글자를 지울 경우 리스트 삭제
     
     let recomList = ["유튜브 프리미엄","유튜브 미리보기","유튜브 검색하는 법"];
-    var search_keywords = $(".showKeywords");
-    //var search_keywords= document.getElementsByClassName("showKeywords")[0];
+    var search_keywords= document.getElementsByClassName("showKeywords")[0];
+    //var search_keywords = $(".showKeywords");
     //var search_keywords = $("search_keywords");
-    function keywordsList()
+    function keywordsList(eqkeywords)
     {
+        rmkeywords();
         for(var i=0; i<recomList.length; i++)
         {
             //console.log(i);
-            var keywords = document.getElementsByClassName("keywords")[0].cloneNode(true);
-            var keywordsContainer = document.getElementById("search_keywords");
-            
-            keywords.querySelector(".keywordName").innerText=recomList[i];
-            keywordsContainer.appendChild(keywords);
+            if(recomList[i].indexOf(eqkeywords)!=-1)
+            {
+                console.log(i);
+                var keywords = document.getElementsByClassName("keywords")[0].cloneNode(true);
+                var keywordsContainer = document.getElementById("search_keywords");
+                
+                keywords.querySelector(".keywordName").innerText=recomList[i];
+                keywordsContainer.appendChild(keywords);
+            }
         }
     }
 
@@ -59,30 +64,42 @@ $( document ).ready(function() {
         {
             rmlist[i].remove();
         }
+
     }
 
     
     searchInput.onkeyup=function()
     {
-        console.log("keyup");
+        //console.log("keyup");
         rmkeywords();
         var searchValue = document.getElementById("input").value;
-        if(searchValue.indexOf("유")!=-1)
+        for(var i=0; i<recomList.length; i++)
         {
-            console.log("ㅇㅇ");
-            keywordsList();
-            search_keywords.addClass("onKeyup");
-            //search_keywords.style.opacity="1";
-            //search_keywords.style.zIndex="2";
-            //search_keywords.attr("id", "onKeyup");
-        }
-        else
-        {
-            console.log("ㄴㄴ");
-            search_keywords.removeClass("onKeyup");
-            //search_keywords.style.opacity="0";
-            //search_keywords.style.zIndex="-2";
-            //search_keywords.removeAttr("id", "onKeyup");
+            if(searchValue=="" || searchValue==" ")
+            {
+                search_keywords.style.opacity="0";
+                search_keywords.style.zIndex="-2";
+            }
+            else if(recomList[i].indexOf(searchValue)!=-1)
+            {
+                console.log(searchValue);
+                console.log("ㅇㅇ");
+                keywordsList(searchValue);
+                //search_keywords.addClass("onKeyup");
+                search_keywords.style.opacity="1";
+                search_keywords.style.zIndex="2";
+                //search_keywords.attr("id", "onKeyup");
+            }
+            /*
+            else
+            {
+                console.log("ㄴㄴ");
+                //search_keywords.removeClass("onKeyup");
+                //rmkeywords();
+                search_keywords.style.opacity="0";
+                search_keywords.style.zIndex="-2";
+                //search_keywords.removeAttr("id", "onKeyup");
+            }*/
         }
         
     }
@@ -91,6 +108,15 @@ $( document ).ready(function() {
     // '유'를 다 쳤을 때 밑에 검색어 리스트 모두 렌더
     // 검색창에 글씨를 다 지웠을 때 리스트 모두 삭제
     
+
+
+
+    //9월 1주차
+    //추천 목록 여러개 추가 
+    // 유동적으로 받아서 검색 추천
+    //검색된 리스트 클릭 시 해당 단어 input 창 세팅 후 검색
+
+
 
 
 
