b2b4af96f8d28460061f61d92622db2a13589c71
hirunaa <tmfrl040622@naver.com>
2022-08-29 17:54:12 +0900

    수정

--- js/text1.js
+++ js/text1.js
@@ -9,19 +9,20 @@ $(document).ready(function () {
     var site_name = ["Instagram", "Chrome", "add"];
 
     let recomList = ["유튜브 프리미엄", "유튜브 미리보기", "유튜브 검색하는 법"];
-    let recomList_2 = ["유튜브","유튜브 프리미엄", "유튜브 미리보기", "유튜브 검색하는 법", "물리2","물리학", "몰?루", "아!루", "리눅스", "스플렁크"];
+    let recomList_2 = ["유튜브","유튜브 프리미엄", "유튜브 미리보기", "유튜브 검색하는 법", "물리2","물리학", "몰?루", "아!루", "리눅스", "스플렁크", ];
 
     var search_keywords = document.getElementsByClassName("showKeywords")[0];
 
     function input_radius_off()
     {
+        searchInput.style.borderTopLeftRadius="11px";
+        searchInput.style.borderTopRightRadius="11px";
         searchInput.style.borderBottomRightRadius = "0";
         searchInput.style.borderBottomLeftRadius = "0";
     }
     function input_radius_on()
     {
-        searchInput.style.borderBottomRightRadius = "22px";
-        searchInput.style.borderBottomLeftRadius = "22px";
+        searchInput.style.borderRadius="22px";
     }
 
     searchInput.onblur = function(e) {
@@ -32,6 +33,7 @@ $(document).ready(function () {
             input_radius_on();
         }
     }
+
     searchInput.onkeydown = function(event){
         if(event.key=="Escape" || event.key=="Tab"){
             container.removeClass("onFocus");
@@ -42,24 +44,17 @@ $(document).ready(function () {
     for (var i = 0; i < icon_src.length; i++) {
         let iconList = document.getElementsByClassName("icon_table")[0].cloneNode(true);
         let iconbox = document.getElementById("icon_container");
-
         iconList.querySelector(".icon").setAttribute('src', icon_src[i]);
         iconList.querySelector(".siteName").innerText = site_name[i];
         iconbox.appendChild(iconList);
     }
     // input창에 "유"입력 시 연관검색어 출력. 글자를 지울 경우 리스트 삭제
-
-    //var search_keywords = $(".showKeywords");
-    //var search_keywords = $("search_keywords");
     function keywordsList(eqkeywords) {
         rmkeywords();
         for (var i = 0; i < recomList_2.length; i++) {
-            //console.log(i);
             if (recomList_2[i].indexOf(eqkeywords) != -1) {
-                console.log(i);
                 var keywords = document.getElementsByClassName("keywords")[0].cloneNode(true);
                 var keywordsContainer = document.getElementById("search_keywords");
-
                 keywords.querySelector(".keywordName").innerText = recomList_2[i];
                 keywordsContainer.appendChild(keywords);
             }
@@ -71,12 +66,9 @@ $(document).ready(function () {
         for (var i = 1; i < rmlist.length; i++) {
             rmlist[i].remove();
         }
-
     }
 
-
     searchInput.onkeyup = function () {
-        //console.log("keyup");
         rmkeywords();
         var searchValue = document.getElementById("input").value;
         for (var i = 0; i < recomList_2.length; i++) {
@@ -87,15 +79,11 @@ $(document).ready(function () {
                 input_radius_on();
             }
             else if (recomList_2[i].indexOf(searchValue) != -1) {
-                //console.log(searchValue);
                 container.addClass("onFocus");
                 input_radius_off();
-                console.log("ㅇㅇ");
                 keywordsList(searchValue);
-                //search_keywords.addClass("onKeyup");
                 search_keywords.style.opacity = "1";
                 search_keywords.style.zIndex = "2";
-                //search_keywords.attr("id", "onKeyup");
             }
         }
         var inputText=document.getElementById("input");
